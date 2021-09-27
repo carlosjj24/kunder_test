5b2c05b77879d1e0b11132812a9cabd83998c4b5
Carlos J <ciglesias@shokworks.io>
2021-09-26 23:10:15 -0400

    heroku 2

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
+app.use(express.static(__dirname+'/dist/Kunder-exam'));
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/Kunder-exam/index.html'));
 });
 
